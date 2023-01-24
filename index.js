f1a95fdcafa99dceb025255fa93706effdf094c8
Gabriel Correa Aielo <68876300+Gabriel-aielo@users.noreply.github.com>
2023-01-24 17:14:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById('mensagem').onclick = function(){
        {
         icon: 'info',
         title: 'Seja bem vindo!...',
-        text: 'Esta página foi programada por Gabriel Corrêa Aielo. As suas informações bem como o layout da página são constantemente atualizadas. Infelizmente, no momento não é possível ainda acessar indivudualmente cada página de projetos da seção "Projetos Programação". Entretanto as medidas cabíveis estão sendo tomadas. Caso houver algum problema com a sua navegação, por favor informar o dono :)  ',
+        text: 'Esta página foi programada por Gabriel Corrêa Aielo. As suas informações bem como o layout da página são constantemente atualizadas. Você pode acessar individualmente cada projeto na aba engenharia ou na aba programação, basta clicar no botão que você será redirecionado. Caso houver algum problema com a sua navegação, por favor informar ao proprietário :)  ',
         footer: '<a href="">Why do I have this issue?</a>'
        }
     )
